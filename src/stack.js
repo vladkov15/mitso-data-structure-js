93355829f28f369a43857b4b3fdef00e696a912a
vladislav_kovaliov <vkov2001@gmai.com>
2021-12-17 22:01:39 +0300

    stack complete

--- src/stack.js
+++ src/stack.js
@@ -13,18 +13,22 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  */
 module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError("Not implemented");
+  constructor(){
+    this.arr = [];
+  }
+
+  push( element ) {
+    this.arr.push(element);
     // remove line with error and write your code here
   }
 
   pop() {
-    throw new NotImplementedError("Not implemented");
+    return this.arr.pop();
     // remove line with error and write your code here
   }
 
   peek() {
-    throw new NotImplementedError("Not implemented");
+    return this.arr[this.arr.length - 1];
     // remove line with error and write your code here
   }
 };
